e2939fbb143ebbfbbcfb56a6a3b27e321c6e63ff
ChenYeh_Chien <samddd6718@gmail.com>
2026-01-20 11:30:08 +0800

    upload new blog content

--- src/components/client/Navbar/Navbar.tsx
+++ src/components/client/Navbar/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar({ blogMenu, handleShowSidebar, showSidebar }: Pro
     <header
       className={clsx(
         "fixed top-0 z-9 w-full bg-(--navbar-bg)",
-        "text-(--navbar-color) px-5 pt-3 flex justify-between"
+        "text-(--navbar-color) px-5 py-3 flex justify-between"
       )}>
       <button
         className="lg:hidden hover:cursor-pointer"
